c93c06bd0e4f2393fba6279b15e2a5b71a2d950e
Mateus <mateus-medeiross@hotmail.com>
2021-01-28 21:35:41 -0300

    Order, Instant, ISO 8601 (part 01)

---
+++ src/main/java/com/course/springCourse/entities/Order.java
@@ -0,0 +1,36 @@
+package com.course.springCourse.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	public Order() {
+		
+	}
+
+	public Order(Long id, Instant moment, User client) {
+		
+	}
+}
