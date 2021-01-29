b2115e2730fa4983870d6e456ceaaa5753ddd32f
Mateus <mateus-medeiross@hotmail.com>
2021-01-29 14:11:31 -0300

    Category, Entity, Resource

--- src/main/java/com/course/springCourse/config/TestConfig.java
+++ src/main/java/com/course/springCourse/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.course.springCourse.entities.Category;
 import com.course.springCourse.entities.Order;
 import com.course.springCourse.entities.User;
 import com.course.springCourse.entities.enums.OrderStatus;
+import com.course.springCourse.repositories.CategoryRepository;
 import com.course.springCourse.repositories.OrderRepository;
 import com.course.springCourse.repositories.UserRepository;
 
@@ -24,10 +26,21 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired	
+	private CategoryRepository categoryRepository;
 
 
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {	
+		
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
