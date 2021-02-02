42e2192c02aa24f437ee0e4ee729321343bf7b98
Mateus <mateus-medeiross@hotmail.com>
2021-02-01 21:58:27 -0300

    Exception handling - delete

--- src/main/java/com/course/springCourse/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/course/springCourse/resources/exceptions/ResourceExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.course.springCourse.services.exceptions.DataBaseException;
 import com.course.springCourse.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -21,6 +22,15 @@ public class ResourceExceptionHandler {
 			HttpServletRequest request){
 		String error = "Resorce not Found ";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, 
+			HttpServletRequest request){
+		String error = "DataBase error ";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/course/springCourse/services/UserService.java
+++ src/main/java/com/course/springCourse/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.course.springCourse.entities.User;
 import com.course.springCourse.repositories.UserRepository;
+import com.course.springCourse.services.exceptions.DataBaseException;
 import com.course.springCourse.services.exceptions.ResourceNotFoundException;
 
 
@@ -33,7 +36,14 @@ public class UserService {
 	
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+
 	}
 	
 	public User update(Long id, User obj) {
